4015995a56312886a6f96a662d153f524657b19a
Patrick Hansen <patrick.hansen@hotmail.com>
2016-09-05 12:40:09 +0200

    Added log and Helpers class

---
+++ GeometricCalculator/GeometricCalculator/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GeometricCalculator
+{
+    class Log
+    {
+        public void log( object result )
+        {
+            //string line = "Type: " + type + " Formula: " + formula + " Result: " + result;
+            string line = "hej";
+            using (StreamWriter w = File.AppendText("C:/Users/"+ Helpers.getActiveUser() +"/Documents/test.txt"))
+            {
+                w.WriteLine(line);
+            }
+        }
+    }
+}
